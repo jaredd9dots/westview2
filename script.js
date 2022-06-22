059c74bce0168ce759b3c0534c0c369321680504
jaredd9dots <108029466+jaredd9dots@users.noreply.github.com>
2022-06-22 14:15:12 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function switchcolor() {
+
+    let element = document.getElementsByClassName("background-red");
+
+    element[0].classList.add("background-purple");
+    element[0].classList.remove("background-red");
+
+}
